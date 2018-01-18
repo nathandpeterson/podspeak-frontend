84851e31a91ec78afd5171bd5dd8877c9b6e1f1c
Nathan Daniel Peterson <natperson@gmail.com>
2018-01-18 13:10:23 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Podspeak
+      </div>
+    );
+  }
+}
+
+export default App
