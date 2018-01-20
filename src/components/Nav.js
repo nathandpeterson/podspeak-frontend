b0c64b653f3eebb10646d98e8790341825342278
Nathan Daniel Peterson <natperson@gmail.com>
2018-01-19 16:15:35 -0800

    add auth0 lock for login

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,72 @@
 import React, { Component } from 'react'
 import { Navbar, NavItem } from 'react-materialize'
+import Auth0Lock from 'auth0-lock'
 
 class Nav extends Component {
+    constructor(){
+        super()
+        this.state = {}
+    }
+
+    static defaultProps = {
+        clientId: 'hXQR6COgtLrGmaV5NUJJdwufjZpmxQC5',
+        domain: 'natperson.auth0.com'
+      }
+    componentDidMount(){
+        this.lock = new Auth0Lock(this.props.clientId, this.props.domain)
+        this.lock.on('authenticated', (authResult)=>{
+            this.lock.getUserInfo(authResult.accessToken, (error, profile)=>{
+                if(error) {
+                  console.log(error)
+                  return
+                }
+            this.setData(authResult.accessToken, profile)
+        })
+        })
+        this.getData()
+    }
+
+    //function for setting token and profile data
+  setData(accessToken, profile){
+    localStorage.setItem('accessToken', accessToken)
+    localStorage.setItem('profile', JSON.stringify(profile))
+    this.setState({
+      accessToken: localStorage.getItem('accessToken'),
+      profile: JSON.parse(localStorage.getItem('profile'))
+    })
+  }
+  getData(){
+    if(localStorage.getItem('accessToken') != null){
+      this.setState({
+        accessToken: localStorage.getItem('accessToken'),
+        profile: JSON.parse(localStorage.getItem('profile'))
+      }, ()=>{
+        console.log(this.state)
+      })
+    }
+  }
+
+  //delete token in state and local storage
+  logout(){
+    this.setState({
+      accessToken: '',
+      profile: ''
+    }, () => {
+      localStorage.removeItem('accessToken')
+      localStorage.removeItem('profile')
+    })
+
+  }
+
+    loginClick = (e) => {
+        e.preventDefault()
+        this.lock.show()
+    }
+
     render () {
         return (
             <Navbar right brand="podspeak">
-                <NavItem> Log In </NavItem>
+                <NavItem onClick={this.loginClick}>log in</NavItem>
             </Navbar>
         )
     }
