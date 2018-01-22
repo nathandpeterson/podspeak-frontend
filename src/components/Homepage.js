da88c94bf1575de4288e0c1a5e0766fe2e098971
Nathan Daniel Peterson <natperson@gmail.com>
2018-01-22 06:16:57 -0800

    add styles to cards

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -19,14 +19,17 @@ query{
 class Homepage extends Component {
     
     render(){
-        console.log(this.props)
         if(!this.props.data.podcasts) return <div />
         const { podcasts } = this.props.data
-        console.log('PODVASTS',podcasts)
         return <div className="main-container"> wtf
             {podcasts.map(podcast => {
-                return <div className="pod-card-container"> <Card key={podcast.id}
-                header={<CardTitle reveal image={podcast.image_URL} waves='light'/>}
+                return <div className="pod-card-container"> 
+                            <Card key={podcast.id}
+                                    className="pod-card"
+                                    header={<CardTitle 
+                                    reveal 
+                                    image={podcast.image_URL} 
+                                    waves='light'/>}
                 title={podcast.title}
                 reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}>
                 <p><a href="#">More...</a></p>
