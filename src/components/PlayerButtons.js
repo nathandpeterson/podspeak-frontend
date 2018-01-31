3e000bd42eb2cc6e6cbbdc9a41b5e281eb8817ad
Nathan Daniel Peterson <natperson@gmail.com>
2018-01-31 10:54:09 -0800

    add screensize media query for button size

--- src/components/PlayerButtons.js
+++ src/components/PlayerButtons.js
@@ -4,6 +4,11 @@ import { Row, Col, Icon } from 'react-materialize'
 
 
 class PlayerButtons extends Component {
+    constructor() {
+        super()
+
+        this.state = {width: ''}
+    }
     renderPlayPause(){
         return this.props.playing ? 'pause' : 'play_arrow'
     }
@@ -19,33 +24,43 @@ class PlayerButtons extends Component {
     forwardThirty = () => {
         this.props.seek(30)
     }
+    componentWillMount(){
+        // Set the state to true if the screen width is larger than 800px
+        const mediaQuery = window.matchMedia("(min-width: 800px)").matches
+        this.setState({width: mediaQuery})
+    }
+    renderButtons = () => {
+        // render large icons on the buttons if it is a full-sized screen
+        return this.state.width ? "large" : "medium"
+    }
+
     render(){
         return <div>
                     <Row>
                         <Col s={1}></Col>
                         <Col s={2}>
                             <div onClick={this.backTen} className="player-btn">
-                                <Icon large>replay_10</Icon>
+                                <i className={`material-icons ${this.renderButtons()}`}>replay_10</i>
                             </div>
                         </Col>
                         <Col s={2}>
                             <div onClick={this.backThirty} className="player-btn">
-                                <Icon large>replay_30</Icon>
+                                <i className={`material-icons ${this.renderButtons()}`}>replay_30</i>
                             </div>
                         </Col>
                         <Col s={2}>
                             <div onClick={this.props.togglePlay} className="player-btn">
-                                <Icon large>{`${this.renderPlayPause()}`}</Icon>
+                                <i className={`material-icons ${this.renderButtons()}`}>{`${this.renderPlayPause()}`}</i>
                             </div>
                         </Col>
                         <Col s={2}>
                             <div onClick={this.forwardTen} className="player-btn">
-                                <Icon large>forward_10</Icon>
+                                <i className={`material-icons ${this.renderButtons()}`} large>forward_10</i>
                             </div>
                         </Col>
                         <Col s={2}>
                             <div onClick={this.forwardThirty} className="player-btn">
-                                <Icon large>forward_30</Icon>
+                                <i className={`material-icons ${this.renderButtons()}`} large>forward_30</i>
                             </div>
                         </Col>
                         <Col s={1}></Col>
--- src/components/ReactionFeed.js
+++ src/components/ReactionFeed.js
@@ -11,27 +11,38 @@ class ReactionFeed extends Component {
         this.state = {textComment: '', commentForm: false}
     }
 
+    formatUserInfo = (data) => {
+        const { userInfo } = data
+        const [ id, avatar , first_name, last_name ] = userInfo.split(',')
+        return { id, avatar, first_name, last_name }
+    }
+
     renderText = (data) => {
-        return <CardPanel key={data.id} className='grey lighten-3 text-card-style'>
-                <Chip>Name</Chip>
-                <div className="text-style"><p>{data.content}</p></div>
+        const userInfo = this.formatUserInfo(data)
+        return <div key={data.id} className='grey lighten-3 text-card-style'>
                 <Chip>{data.episode_timestamp}</Chip>
-                </CardPanel>
+                <div className="text-style">
+                    <p>{data.content}</p>
+                </div>
+                <Chip>{userInfo.avatar} {userInfo.first_name}</Chip>
+                </div>
     }
 
     renderEmoji = (data) => {
+        const userInfo = this.formatUserInfo(data)
         return <CardPanel key={data.id} className="image-container-style grey lighten-3" >
-                    <Chip>Name</Chip>
+                    <Chip>{userInfo.avatar} {userInfo.first_name}</Chip>
                     <div className="reaction-emoji"><span style={{fontSize: '3rem' }}>{data.content}</span></div>
                     <Chip>{data.episode_timestamp}</Chip>
                 </CardPanel>
     }
 
     renderImage = (data) => {
+        const userInfo = this.formatUserInfo(data)
         return <CardPanel key={data.id} className="grey lighten-3 image-container-style"  > 
                     <img alt={data.content} className="reaction-image" src={data.content}/>
                     <span style={{display:'block'}}>
-                        <Chip>Name</Chip>
+                    <Chip>{userInfo.avatar} {userInfo.first_name}</Chip>
                         <Chip>{data.episode_timestamp}</Chip>
                        
                     </span>
@@ -57,7 +68,6 @@ class ReactionFeed extends Component {
     submitText = (e) => {
         e.preventDefault()
         // I need content, user_id, episode_id, podcast_id, episode_timestamp, category, ?reaction_id
-        // if I have the episode, I can find the podcast_id on the backend
         let user_id = localStorage.getItem('data')
         if(!user_id) console.log('handle err, you are not logged in')
         this.props.mutate({variables : {
@@ -67,7 +77,7 @@ class ReactionFeed extends Component {
             episode_timestamp: '00:10',
             category: 1}
         }).then(res => {
-            console.log('in the results',this.props)
+            // Need to refetch here!!!!!
             return res
         })
     }
@@ -82,9 +92,8 @@ class ReactionFeed extends Component {
                     <Row>
                         <Col s={1}></Col>
                         <Col s={10}>
-                        <div className="center reaction-feed-container">
+                        <div className=" center reaction-feed-container">
                                 {reactions.map(reaction => {
-                                    console.log(reaction)
                                     const { category } = reaction
                                     if(category === 1) return this.renderText(reaction)
                                     if(category === 2) return this.renderEmoji(reaction)
